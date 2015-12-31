eeaf4b83dc10486681c1740cb791dd58ac4cbd3b
Lionel Ains <lains@gmx.net>
2015-12-31 10:57:18 +0100

    Updating pack.pl with author

--- pack.pl
+++ pack.pl
@@ -1,4 +1,7 @@
 name('link-grammar-binding').
 title('A binding to use the Link Grammar library from SWI-Prolog').
 version('4.1').
-home('https://github.com/lains/swi-prolog-lg').
+home('https://github.com/lains/swi-prolog-lg').author(Name, Email)
+author('Lionel Ains', 'lains@gmx.net').
+maintainer('Lionel Ains', 'lains@gmx.net').
+packager('Lionel Ains', 'lains@gmx.net').
