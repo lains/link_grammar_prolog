e27eb3a50d8ef5936ff2c810d09d73e907c211a6
Lionel Ains <lains@gmx.net>
2016-01-05 18:19:01 +0100

    Fixing pack.pl syntax

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name('link-grammar-binding').
 title('A binding to use the Link Grammar library from SWI-Prolog').
 version('4.1').
-home('https://github.com/lains/swi-prolog-lg').author(Name, Email)
+home('https://github.com/lains/swi-prolog-lg').
 author('Lionel Ains', 'lains@gmx.net').
 maintainer('Lionel Ains', 'lains@gmx.net').
 packager('Lionel Ains', 'lains@gmx.net').
