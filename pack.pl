476c1c10a371930b21a586857755543f02a733a0
Lionel Ains <lains@gmx.net>
2016-01-17 22:07:50 +0100

    Adding download URL in pack.pl

--- pack.pl
+++ pack.pl
@@ -1,7 +1,8 @@
 name('link-grammar-binding').
 title('A binding to use the Link Grammar library from SWI-Prolog').
-version('4.1.0.1').
+version('4.1.0.2').
 home('https://github.com/lains/swi-prolog-lg').
 author('Lionel Ains', 'lains@gmx.net').
 maintainer('Lionel Ains', 'lains@gmx.net').
 packager('Lionel Ains', 'lains@gmx.net').
+download('https://github.com/lains/swi-prolog-lg/archive/4.1.0.2.zip').
