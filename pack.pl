8bcd940ed8505086f1907dc1ff9b947eb388284a
Lionel Ains <lains@gmx.net>
2016-01-17 23:23:22 +0100

    Upgrading to v4.1.0.2

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name('link_grammar_prolog').
 title('A binding to use the Link Grammar library from SWI-Prolog').
-version('4.1.0.1').
+version('4.1.0.2').
 home('https://github.com/lains/link_grammar_prolog').
 author('Lionel Ains', 'lains@gmx.net').
 maintainer('Lionel Ains', 'lains@gmx.net').
 packager('Lionel Ains', 'lains@gmx.net').
-download('https://github.com/lains/link_grammar_prolog/releases/download/link_grammar_prolog-4.1.0.1/link_grammar_prolog-4.1.0.1.zip').
+download('https://github.com/lains/link_grammar_prolog/releases/download/link_grammar_prolog-4.1.0.2/link_grammar_prolog-4.1.0.2.zip').
