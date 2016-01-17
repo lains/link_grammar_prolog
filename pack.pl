5cb12f597877c1eb92462d410e772175bb4f0759
Lionel Ains <lains@gmx.net>
2016-01-17 22:21:06 +0100

    Renaming pack to linkgrammar and using wildcard on version for download URL

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
-name('link-grammar-binding').
+name('linkgrammar').
 title('A binding to use the Link Grammar library from SWI-Prolog').
-version('4.1.0.2').
+version('4.1.0.3').
 home('https://github.com/lains/swi-prolog-lg').
 author('Lionel Ains', 'lains@gmx.net').
 maintainer('Lionel Ains', 'lains@gmx.net').
 packager('Lionel Ains', 'lains@gmx.net').
-download('https://github.com/lains/swi-prolog-lg/archive/4.1.0.2.zip').
+download('https://github.com/lains/swi-prolog-lg/archive/linkgrammar-*.zip').
