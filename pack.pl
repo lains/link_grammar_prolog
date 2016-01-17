9733092f77b8491df2a1d4af3d03c880cfe13acf
Lionel Ains <lains@gmx.net>
2016-01-17 22:30:54 +0100

    Updating pack.pl to reflect new repo name

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
-name('linkgrammar').
+name('link_grammar_prolog').
 title('A binding to use the Link Grammar library from SWI-Prolog').
-version('4.1.0.3').
-home('https://github.com/lains/swi-prolog-lg').
+version('4.1.0.1').
+home('https://github.com/lains/link_grammar_prolog').
 author('Lionel Ains', 'lains@gmx.net').
 maintainer('Lionel Ains', 'lains@gmx.net').
 packager('Lionel Ains', 'lains@gmx.net').
-download('https://github.com/lains/swi-prolog-lg/archive/linkgrammar-*.zip').
+download('https://github.com/lains/link_grammar_prolog/archive/link_grammar_prolog-*.zip').
