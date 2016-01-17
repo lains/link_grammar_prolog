7549b477918643ef44aac60331f9b56bf9301ae0
Lionel Ains <lains@gmx.net>
2016-01-17 22:49:31 +0100

    Adapting pack.pl URLs to format expected by SWI-Prolog

--- pack.pl
+++ pack.pl
@@ -5,4 +5,4 @@ home('https://github.com/lains/link_grammar_prolog').
 author('Lionel Ains', 'lains@gmx.net').
 maintainer('Lionel Ains', 'lains@gmx.net').
 packager('Lionel Ains', 'lains@gmx.net').
-download('https://github.com/lains/link_grammar_prolog/archive/link_grammar_prolog-*.zip').
+download('https://github.com/lains/link_grammar_prolog/releases/download/link_grammar_prolog-4.1.0.1/link_grammar_prolog-4.1.0.1.zip').
