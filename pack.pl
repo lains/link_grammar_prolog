17fda2d60fe3661b0386de7a50d78ca6120b05a2
Lionel Ains <lains@gmx.net>
2016-01-17 22:01:10 +0100

    Updating version to release 4.1.0.1

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name('link-grammar-binding').
 title('A binding to use the Link Grammar library from SWI-Prolog').
-version('4.1').
+version('4.1.0.1').
 home('https://github.com/lains/swi-prolog-lg').
 author('Lionel Ains', 'lains@gmx.net').
 maintainer('Lionel Ains', 'lains@gmx.net').
